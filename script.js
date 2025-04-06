66ff2c12e31577f370373f2553365a77219ae2bc
damianodionori <d.dionori@gmail.com>
2025-04-06 23:09:36 +0200

    mappa

--- script.js
+++ script.js
@@ -158,4 +158,21 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 300) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+
+    scrollToTopBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
 });
